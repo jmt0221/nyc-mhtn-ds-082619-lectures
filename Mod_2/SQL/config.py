2e519fea4a604ba00a0d37a48ec35c2da3abaa80
Justin <jmtennenbaum@gmail.com>
2019-09-12 11:03:39 -0400

    paul got mysql working

---
+++ Mod_2/SQL/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 12:00:49 2019
+
+@author: justintennenbaum
+"""
+
+
+host = 'flatiron.cjvonmhiys8h.us-east-2.rds.amazonaws.com'
+user='justin6265'
+passwd='flatiron6265'
+
