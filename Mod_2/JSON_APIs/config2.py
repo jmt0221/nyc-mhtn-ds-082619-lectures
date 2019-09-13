5e2053bc3d431b9915c61f5296b00336d40a5f3e
Justin <jmtennenbaum@gmail.com>
2019-09-13 10:08:18 -0400

    students

---
+++ Mod_2/JSON_APIs/config2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 10:06:15 2019
+
+@author: justintennenbaum
+"""
+
+
+host = 'database-1.cjvonmhiys8h.us-east-2.rds.amazonaws.com'
+user='justin6265'
+passwd='flatiron6265'
